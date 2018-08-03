c7312ad8ff4179c7df20f6a27bf5284ab516056b
suraj.n.gupta <suraj.n.gupta@accenture.com>
2018-08-03 01:18:46 +0100

    Ui changes.

--- src/app/components/validate-email/validate-email.component.ts
+++ src/app/components/validate-email/validate-email.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnDestroy,
+  TemplateRef} from '@angular/core';
 import { User } from '../../models/user.model';
 import {
   ReactiveFormsModule,
@@ -11,6 +16,10 @@ import {
   AbstractControl,
 } from '@angular/forms';
 
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { Observable } from "rxjs/Observable";
+
 @Component({
   selector: 'app-validate-email',
   templateUrl: './validate-email.component.html',
@@ -21,20 +30,29 @@ export class ValidateEmailComponent implements OnInit {
   @Input()
   public user;
   public myform: FormGroup;
-  //public email: FormControl;
+  public modalRef: BsModalRef;
 
-  constructor() { }
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
     //this.createFormControls();
     this.createForm();
   }
 
+
+    /**
+     * to open modal dailog
+     */
+    public openModal(template: TemplateRef < any >): void {
+        this.modalRef = this.modalService.show(template);
+    }
+
   public createForm() {
+    const validEmailRegEx = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
     this.myform = new FormGroup({
       email: new FormControl('', [
         Validators.required,
-        Validators.pattern("[^ @]*@[^ @]*"),
+        Validators.pattern(validEmailRegEx),
         this.emailMatchValidator.bind(this)
       ])
     }, { updateOn: 'blur' });
@@ -47,11 +65,11 @@ export class ValidateEmailComponent implements OnInit {
   public emailMatchValidator(control: AbstractControl): { [key: string]: boolean } {
     console.log(this.user.santa.email);
     const emailEntered = control.value;
-    if (emailEntered && emailEntered.indexOf("@") != -1) {
+    if (emailEntered && (control.errors && !control.errors.pattern)) {
       const forbidden = emailEntered !== this.user.santa.email;
-      return forbidden ? { 'matchEmail': forbidden } : null;
+      return {'matchEmail': forbidden };
     }
-    return null;
+    return {'matchEmail': false };
   }
 
 }
